a67ef3945751c73525aba7434646aefb1365371a
vijay-sankar-pamidimarri <sankerpamidimarri@gmail.com>
2024-09-13 09:31:04 +0530

    i comitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
-
+class Product {
+    public int product(int x,int y){
+            return x*y;
+    }
+    public int product(int x,int y,int z){
+            return x*y*z;
+    }
+    public double product(double x,double y){
+            return x*y;
+    }
+}
 public class Main {
 
+public static void main(String[] args){
+    Product p=new Product();
+    p.product(2,3);
+    p.product(2,3,4);
+    p.product(2.1,3.4);
+}
 }
